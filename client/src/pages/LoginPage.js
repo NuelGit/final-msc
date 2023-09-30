f023f5e7ce0e55f3f79192673374e0219e1dd93b
NuelGit <zioneemma@gmail.com>
2023-09-30 21:26:53 +0100

    updated

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -62,48 +62,5 @@ const LoginPage = () => {
   );
 };
 
-// const history = useHistory();
-// const [email, setEmail] = useState('');
-// const [password, setPassword] = useState('');
-// const [error, setError] = useState(null);
-
-// const handleLogin = async (e) => {
-//   e.preventDefault();
-
-//   try {
-//     await firebase.auth().signInWithEmailAndPassword(email, password);
-//     history.push('/dashboard'); // Redirect to the dashboard after successful login
-//   } catch (err) {
-//     setError(err.message);
-//   }
-// };
-
-// return (
-//   <div className="login-container">
-//     <h2>Login</h2>
-//     <form onSubmit={handleLogin}>
-//       <div className="form-group">
-//         <label>Email:</label>
-//         <input
-//           type="email"
-//           placeholder="Email"
-//           value={email}
-//           onChange={(e) => setEmail(e.target.value)}
-//         />
-//       </div>
-//       <div className="form-group">
-//         <label>Password:</label>
-//         <input
-//           type="password"
-//           placeholder="Password"
-//           value={password}
-//           onChange={(e) => setPassword(e.target.value)}
-//         />
-//       </div>
-//       <button type="submit">Login</button>
-//     </form>
-//     {error && <p className="error-message">{error}</p>}
-//   </div>
-// );
 
 export default LoginPage;
